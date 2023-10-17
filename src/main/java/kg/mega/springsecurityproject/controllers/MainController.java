4e5a6acc4b8f6900e6f99b511e93c133c15e3494
Emir Bolotbekov <bolotbekove32@gmail.com>
2023-10-17 20:41:37 +0600

    Реализация MainController

---
+++ src/main/java/kg/mega/springsecurityproject/controllers/MainController.java
@@ -0,0 +1,32 @@
+package kg.mega.springsecurityproject.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/unsecured")
+    public String unSecureData() {
+        return "Unsecure data";
+    }
+
+    @GetMapping("/secured")
+    public String secureData() {
+        return "Secure data";
+    }
+
+    @GetMapping("/admin")
+    public String adminData() {
+        return "Admin data";
+    }
+
+    @GetMapping("/info")
+    public String userData(Principal principal) {
+        return principal.getName();
+    }
+}
